bf036e94e5da5f71f17487345dcdefa6bb367c50
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-08-16 10:08:38 +0600

    add new

--- index.js
+++ index.js
@@ -12,6 +12,46 @@ app.use(express.json());
 
 app.post("/gen", async (req, res) => {
   const {url} = req.body;
+  console.log('get a req from:',url);
+
+  if(!url){
+    return res.status(404).send('no url pass')
+  }
+
+  try {
+    
+        // genarate pdf 
+        const browser = await puppeteer.launch({
+            headless: "new",
+            executablePath:
+              process.env.NODE_ENV === "production"
+                ? process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath(),
+          });
+
+          const page = await browser.newPage();
+
+          const loaddata = await page.goto(url, {
+            waitUntil: "networkidle2",
+          });
+
+          await page.setViewport({ width: 2480, height: 3508 });
+
+          const pdfBuffer = await page.pdf({ format: 'A4' });
+
+          await browser.close();
+          res.contentType('application/pdf');
+          res.send(pdfBuffer);
+          
+
+  } catch (error) {
+    console.log("🚀 ~ file: index.js:42 ~ app.get ~ error:", error);
+    res.send("eror");
+  }
+});
+app.get("/test", async (req, res) => {
+  const url = "https://nahidhasan141400.github.io/banglaos/"
+  console.log('get a req from:',url);
 
   if(!url){
     return res.status(404).send('no url pass')
